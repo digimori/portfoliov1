860a6843b9215f21868f69e3c20194a75c903294
digimori <hanaorikuu@gmail.com>
2023-10-01 16:34:46 +0000

    Cursor animation tested. Unsure if I want to keep it - Currently commented out

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-//alert("Working")
-
 const offcanvasElementList = document.querySelectorAll('.offcanvas')
 const offcanvasList = [...offcanvasElementList].map(offcanvasEl => new bootstrap.Offcanvas(offcanvasEl))
+
+/*
+Animated Cursor project
+const cursor = document.querySelector(".cursor");
+
+//follow cursor on mouse movement
+document.addEventListener("mousemove", (e) => {
+  let x = e.pageX;
+  let y = e.pageY;
+
+  cursor.style.top = y + "px";
+  cursor.style.left = x + "px";
+  cursor.style.display = "block";
+
+  //cursor effect on mouseout
+  document.addEventListener("mouseout", () => {
+    cursor.style.display = "none";
+  }
+  )
+
+
+})*/
