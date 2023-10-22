1bb77aa3c88bc2f6acae8ef646ed7fcfa1a37ddd
digimori <hanaorikuu@gmail.com>
2023-10-22 14:39:27 +0000

    Padding removed from hero section on small screens to center it

--- script.js
+++ script.js
@@ -24,3 +24,12 @@ document.addEventListener("mousemove", (e) => {
 
 
 })*/
+
+
+
+let mybutton = document.getElementById("myBtn");
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
