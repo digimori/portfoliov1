f6237c210598066aa6b7821c35a8b88b64c74d20
digimori <hanaorikuu@gmail.com>
2023-11-09 20:57:15 +0000

    Moved all scripts into external JS file. Fixed Offcanvas bug - kept scrolling up on close. Added animation to about.

--- script.js
+++ script.js
@@ -1,35 +1,37 @@
+const scrollTopBtn = document.getElementById("scrollTop");
 
-
-const offcanvasElementList = document.querySelectorAll('.offcanvas')
-const offcanvasList = [...offcanvasElementList].map(offcanvasEl => new bootstrap.Offcanvas(offcanvasEl))
-
-/*
-Animated Cursor project
-const cursor = document.querySelector(".cursor");
-
-//follow cursor on mouse movement
-document.addEventListener("mousemove", (e) => {
-  let x = e.pageX;
-  let y = e.pageY;
-
-  cursor.style.top = y + "px";
-  cursor.style.left = x + "px";
-  cursor.style.display = "block";
-
-  //cursor effect on mouseout
-  document.addEventListener("mouseout", () => {
-    cursor.style.display = "none";
-  }
-  )
-
-
-})*/
-
-
-
-let mybutton = document.getElementById("myBtn");
-
-function topFunction() {
+scrollTopBtn.addEventListener("click", function () {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+});
+
+
+// Initializer for EmailJS
+(function () {
+  emailjs.init('h3mwLcn-TnDz6jnIj');
+})();
+
+// Form validation and sender for EmailJS
+window.onload = function () {
+  document.getElementById('myForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    emailjs.sendForm('service_x7z95au', 'template_h79m2xb', '#myForm', 'h3mwLcn-TnDz6jnIj')
+      .then(function () {
+        console.log('SUCCESS!');
+      }, function (error) {
+        console.log('FAILED...', error);
+      });
+  });
 }
+
+
+// Data toggler for Offcanvas - Bootstrap default behaviour scrolls back to top on close, this is to stop it.
+document.addEventListener("DOMContentLoaded", function(){
+  var myOffcanvas = document.getElementById('offcanvasScrolling');
+  var bsOffcanvas = new bootstrap.Offcanvas(myOffcanvas);
+  document.getElementById("openMenu").addEventListener('click',function (e){
+    e.preventDefault();
+    e.stopPropagation();
+    bsOffcanvas.toggle();
+  });
+});
